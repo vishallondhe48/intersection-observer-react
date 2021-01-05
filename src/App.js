0496bec810ac973ea250018f617540b772f4f8a1
vishallondhe48 <vtechgeneration@gmail.com>
2021-01-05 20:38:54 +0530

    starting of intersection observer for react

---
+++ src/App.js
@@ -0,0 +1,52 @@
+
+import React from 'react'
+import './styles.scss'
+
+
+const App = () => {
+  return (
+    <>
+
+      <h1>intersection-observer-react</h1>
+      <div className="parent container">
+        <div className="box box-1">
+
+        </div>
+        <div className="box box-2">
+
+        </div>
+      </div>
+      <div className="parent container">
+        <div className="box box-3">
+
+        </div>
+        <div className="box box-4">
+
+        </div>
+      </div>
+      <div className="parent container">
+        <div className="box box-1">
+
+        </div>
+        <div className="box box-2">
+
+        </div>
+      </div>
+      <div className="parent container">
+        <div className="box box-3">
+
+        </div>
+        <div className="box box-4">
+
+        </div>
+      </div>
+
+
+
+
+
+    </ >
+  )
+}
+
+export default App
