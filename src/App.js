6e0bfe8ba57d0e6996b3b83051a506c86ffb1d23
vishallondhe48 <vtechgeneration@gmail.com>
2021-01-05 20:48:10 +0530

    created the base template for intersection observer

--- src/App.js
+++ src/App.js
@@ -8,42 +8,42 @@ const App = () => {
     <>
 
       <h1>intersection-observer-react</h1>
-      <div className="parent container">
-        <div className="box box-1">
 
+      <div className="parent container parent-1">
+        <div className="box box-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus unde esse quo vero suscipit corrupti obcaecati tempora veniam. Nemo illo rem dolores quaerat ea? Consequatur illo perspiciatis doloremque at repudiandae ab vel iure consequuntur ea soluta pariatur delectus, dolor ipsum nihil ducimus quam fugiat nesciunt cumque obcaecati? Quidem eligendi quam perferendis voluptatem debitis quod! Repudiandae facilis dicta aliquid magnam, eum quae fuga cum doloribus iure animi corporis quis doloremque, ipsa nostrum perspiciatis corrupti voluptatum libero ea neque error amet delectus! Voluptatum nobis laborum commodi itaque, harum non tempora dignissimos neque rem quis suscipit explicabo provident rerum cumque recusandae. Modi, sint.
         </div>
         <div className="box box-2">
 
         </div>
       </div>
-      <div className="parent container">
+
+      <div className="parent container parent-2">
         <div className="box box-3">
 
         </div>
         <div className="box box-4">
-
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, ducimus. Doloribus minus a aperiam ullam, enim mollitia obcaecati maxime nihil maiores ducimus sequi ipsa nesciunt totam voluptas id! Facere, deserunt.
         </div>
       </div>
-      <div className="parent container">
-        <div className="box box-1">
 
+      <div className="parent container parent-3">
+        <div className="box box-1">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores reiciendis ab quos quae, fuga eos facilis nam dolore in natus eveniet tempora autem officiis corrupti fugiat, temporibus neque aliquid dignissimos accusantium. Reiciendis perspiciatis quidem placeat, dignissimos accusamus error dolorem assumenda magnam, rem nobis dolorum magni repellendus recusandae facere autem accusantium.
         </div>
         <div className="box box-2">
 
         </div>
       </div>
-      <div className="parent container">
+
+      <div className="parent container parent-4">
         <div className="box box-3">
 
         </div>
         <div className="box box-4">
-
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores reiciendis ab quos quae, fuga eos facilis nam dolore in natus eveniet tempora autem officiis corrupti fugiat, temporibus neque aliquid dignissimos accusantium. Reiciendis perspiciatis quidem placeat, dignissimos accusamus error dolorem assumenda magnam, rem nobis dolorum magni repellendus recusandae facere autem accusantium.
         </div>
       </div>
-
-
-
-
 
     </ >
   )
